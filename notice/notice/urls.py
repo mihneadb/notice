b3ee35ec77e65b2071b53f016f8a2d8ace2d8205
Mihnea Dobrescu-Balaur <mihneadb@gmail.com>
2013-04-15 14:06:38 +0300

    login logout

--- notice/notice/urls.py
+++ notice/notice/urls.py
@@ -10,6 +10,12 @@ urlpatterns = patterns('',
     # url(r'^$', 'notice.views.home', name='home'),
     # url(r'^notice/', include('notice.foo.urls')),
     url(r'^$', HomePageView.as_view()),
+    url(r'^login/$', 'django.contrib.auth.views.login', {
+        'template_name': 'login.html',
+    }),
+    url(r'^logout/$', 'django.contrib.auth.views.logout', {
+        'next_page': '/',
+    }),
 
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
