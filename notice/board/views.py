25f2682e85cc7e4689befcd6f811d446aa0ecb60
Mihnea Dobrescu-Balaur <mihneadb@gmail.com>
2013-04-15 16:36:34 +0300

    post add view WIP

--- notice/board/views.py
+++ notice/board/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404, render_to_response
 from django.template import RequestContext
+from django.contrib.auth.decorators import login_required
 
 from models import Post, Comment
 from forms import PostForm
@@ -9,6 +10,22 @@ from forms import PostForm
 def homepage(request):
     posts = Post.objects.order_by('-date')
     data = {
-        'posts': posts
+        'posts': posts,
     }
     return render_to_response('homepage.html', data, context_instance=RequestContext(request))
+
+@login_required
+def add_post(request):
+    if request.method == 'GET':
+        form = PostForm()
+    else:
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.user = request.user
+            post.save()
+
+    data = {
+        'form':form,
+    }
+    return render_to_response('add_post.html', data, context_instance=RequestContext(request))
